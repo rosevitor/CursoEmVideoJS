ba9acbfb5cb9eaa27321debe481f60b78b03cb08
rosevitor <rosenbaum.maia.vitor@hotmail.com>
2024-02-27 15:16:03 -0300

    Alteração no comentário

--- aula11/ex009.js
+++ aula11/ex009.js
@@ -1,6 +1,6 @@
 let pais = "Brasil"
 console.log(`Vivendo em ${pais}`)
-if (pais == "Brasil"){// Condição dupla!
+if (pais == "Brasil"){// Condição composta!
     console.log("Você é brasileiro!")
 } else {
     console.log("Você é estrangeiro!")
