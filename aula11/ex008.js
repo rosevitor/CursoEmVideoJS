6313b4d82bb764589316e68d904668b3312cfd50
rosevitor <rosenbaum.maia.vitor@hotmail.com>
2024-02-27 05:35:25 -0300

    Até aula 11 feita

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+let vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}KM/H`)
+if (vel>60){// Condição simples!
+    console.log("Você ultrapassou a velocidade permitida. MULTADO!")
+}
+console.log("Dirija sempre usando cinto de segurança")
